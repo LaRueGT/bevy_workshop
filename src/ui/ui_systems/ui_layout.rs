51d6852bde0cb049351d7fe2f34d6b908897a364
LaRueGT <gtlarue@gmail.com>
2024-04-18 11:44:48 -0400

    main menu buttons

--- src/ui/ui_systems/ui_layout.rs
+++ src/ui/ui_systems/ui_layout.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
-use crate::ui::ui_components::MainMenu;
+use bevy::text::BreakLineOn;
+use crate::ui::ui_components::*;
+use crate::ui::ui_styles::*;
 
 pub fn spawn_main_menu(
     mut commands: Commands,
@@ -21,6 +23,11 @@ pub fn build_main_menu(
     let main_menu_entity = commands.spawn(
         (NodeBundle {
             style: Style {
+                flex_direction: FlexDirection::Column,
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                row_gap: Val::Px(8.0),
+                column_gap: Val::Px(8.0),
                 width: Val::Percent(100.0),
                 height: Val::Percent(100.0),
                 ..default()
@@ -30,6 +37,117 @@ pub fn build_main_menu(
         },
          MainMenu {},
         ))
+        .with_children(|parent| {
+            //title
+            parent.spawn(
+                NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::Row,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        width: Val::Px(300.0),
+                        height: Val::Px(120.0),
+                        ..default()
+                    },
+                    ..default()
+                }).with_children(|parent| {
+                //image1
+                parent.spawn(
+                    ImageBundle {
+                        style: Style {
+                            width: Val::Px(64.0),
+                            height: Val::Px(64.0),
+                            margin: UiRect::new(Val::Px(8.0), Val::Px(8.0), Val::Px(8.0), Val::Px(8.0)),
+                            ..default()
+                        },
+                        image: asset_server.load("sprites/ball_blue_large.png").into(),
+                        ..default()
+                    });
+                //text
+                parent.spawn(TextBundle {
+                    text: Text {
+                        sections: vec![TextSection::new(
+                            "Space Busters!",
+                            TextStyle {
+                                font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                font_size: 64.0,
+                                color: Color::rgb(1.0, 1.0, 1.0),
+                            },
+                        )],
+                        justify: JustifyText::Center,
+                        linebreak_behavior: BreakLineOn::NoWrap,
+                        ..default()
+                    },
+                    ..default()
+                });
+                //image2
+                parent.spawn(
+                    ImageBundle {
+                        style: Style {
+                            width: Val::Px(64.0),
+                            height: Val::Px(64.0),
+                            margin: UiRect::new(Val::Px(8.0), Val::Px(8.0), Val::Px(8.0), Val::Px(8.0)),
+                            ..default()
+                        },
+                        image: asset_server.load("sprites/ball_red_large.png").into(),
+                        ..default()
+                    });
+            });
+            //play button
+            parent.spawn((
+                ButtonBundle {
+                    style: BUTTON_STYLE,
+                    background_color: NORMAL_BUTTON_COLOR.into(),
+                    ..default()
+                },
+                PlayButton {},
+            ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Play",
+                                TextStyle {
+                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                    font_size: 32.0,
+                                    color: Color::rgb(1.0, 1.0, 1.0),
+                                },
+                            )],
+                            justify: JustifyText::Center,
+                            linebreak_behavior: BreakLineOn::NoWrap,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+            //quit button
+            parent.spawn((
+                ButtonBundle {
+                    style: BUTTON_STYLE,
+                    background_color: NORMAL_BUTTON_COLOR.into(),
+                    ..default()
+                },
+                QuitButton {},
+            ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle {
+                        text: Text {
+                            sections: vec![TextSection::new(
+                                "Quit",
+                                TextStyle {
+                                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                                    font_size: 32.0,
+                                    color: Color::rgb(1.0, 1.0, 1.0),
+                                },
+                            )],
+                            justify: JustifyText::Center,
+                            linebreak_behavior: BreakLineOn::NoWrap,
+                            ..default()
+                        },
+                        ..default()
+                    });
+                });
+        })
         .id();
     main_menu_entity
 }
