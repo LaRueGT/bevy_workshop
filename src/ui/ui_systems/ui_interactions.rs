7ff477e80d49576542b30c5041ada9bd3349a097
LaRueGT <gtlarue@gmail.com>
2024-04-18 14:39:15 -0400

    main menu button states

--- src/ui/ui_systems/ui_interactions.rs
+++ src/ui/ui_systems/ui_interactions.rs
@@ -1,4 +1,6 @@
+use bevy::app::AppExit;
 use bevy::prelude::*;
+use crate::AppState;
 use crate::ui::ui_components::*;
 use crate::ui::ui_styles::*;
 
@@ -6,12 +8,14 @@ pub fn interact_with_play_button(
     mut button_query: Query<
         (&Interaction, &mut BackgroundColor),
         (Changed<Interaction>, With<PlayButton>)
-    >
+    >,
+    mut app_state_next_state: ResMut<NextState<AppState>>,
 ) {
     if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
         match *interaction {
             Interaction::Pressed => {
                 *background_color = PRESSED_BUTTON_COLOR.into();
+                app_state_next_state.set(AppState::Game);
             }
             Interaction::Hovered => {
                 *background_color = HOVERED_BUTTON_COLOR.into();
@@ -27,12 +31,14 @@ pub fn interact_with_quit_button(
     mut button_query: Query<
         (&Interaction, &mut BackgroundColor),
         (Changed<Interaction>, With<QuitButton>)
-    >
+    >,
+    mut app_exit_event_writer: EventWriter<AppExit>,
 ) {
     if let Ok((interaction, mut background_color)) = button_query.get_single_mut() {
         match *interaction {
             Interaction::Pressed => {
                 *background_color = PRESSED_BUTTON_COLOR.into();
+                app_exit_event_writer.send(AppExit);
             }
             Interaction::Hovered => {
                 *background_color = HOVERED_BUTTON_COLOR.into();
